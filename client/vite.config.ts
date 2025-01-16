f26bafa077b78a5ef9e117503f111cdb74c5a9bb
Liam Rayback <liam@macbook.local>
2025-01-16 09:50:22 -0700

    updates for deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,19 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/api': {
-        target: process.env.VITE_DB_URL,
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: process.env.VITE_DB_URL,
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       },
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,11 +5,17 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.VITE_DB_URL
+const sequelize = process.env.VITE_DB_URL 
   ? new Sequelize(process.env.VITE_DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      // host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
       dialect: 'postgres',
       dialectOptions: {
+        ssl : {
+          require: true,
+          rejectUnauthorized: false,
+        },
         decimalNumbers: true,
       },
     });
