befb860c7f0f3872a1230a5ab95619c64fa37d8a
Liam Rayback <liam@macbook.local>
2025-01-16 07:58:09 -0700

    updated vite config for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,12 +10,12 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: process.env.DB_URL || 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001',
+        target: process.env.DB_URL || 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       },
