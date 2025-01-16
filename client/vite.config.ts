0c2d97d54f78d47ba215b94d1388d70ddcd06022
Liam Rayback <liam@macbook.local>
2025-01-16 08:45:34 -0700

    comment out whole proxy section on viteconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,17 +8,17 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        // target: process.env.VITE_DB_URL || 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        // target: process.env.VITE_DB_URL || 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: process.env.VITE_API_BASE_URL,
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   '/auth': {
+    //     target: process.env.VITE_API_BASE_URL,
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    // },
   },
 });
